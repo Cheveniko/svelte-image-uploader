b5cd8c6f7a951dc6a294d9ef52fdc31520608fd8
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2025-06-02 00:46:03 -0500

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import "../app.css";
+  import { injectAnalytics } from "@vercel/analytics/sveltekit";
   import { Toaster } from "$lib/components/ui/sonner/index.js";
 
   let { children } = $props();
+  injectAnalytics();
 </script>
 
 <Toaster richColors closeButton />
